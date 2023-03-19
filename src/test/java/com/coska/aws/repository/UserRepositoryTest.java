4a02d29f593c3489de48bc558cfa20524d085db5
kwon96 <kwon96@gmail.com>
2023-03-19 00:40:41 -0400

    Clean up

--- src/test/java/com/coska/aws/repository/UserRepositoryTest.java
+++ src/test/java/com/coska/aws/repository/UserRepositoryTest.java
@@ -10,12 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.util.ObjectUtils;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
