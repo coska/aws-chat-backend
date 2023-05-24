77178efabcda6bd18731b5ea118068a05d8c9405
Junyoung Choi <ccjy1113@gmail.com>
2023-05-24 11:46:16 +0900

    feat: add profiles for dev with supporting http

--- src/main/java/com/coska/aws/AwsApplication.java
+++ src/main/java/com/coska/aws/AwsApplication.java
@@ -1,16 +1,40 @@
 package com.coska.aws;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class AwsApplication {
 
+	@Value("${http.port}")
+	private int httpPort;
+
     public static void main(String[] args) {
         SpringApplication.run(AwsApplication.class, args);
     }
+
+	// Let's configure additional connector to enable support for both HTTP and HTTPS
+	@Bean
+    @Profile("dev")
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(httpPort);
+		return connector;
+	}
 
     // @Bean
     // public WebMvcConfigurer corsConfigurer() {
